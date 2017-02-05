7caaa9a588c332b64e8c1047520b54e6fde457c3
s-shimotsumagari <s.shohei0814@gmail.com>
2017-02-05 20:25:56 +0900

    IButton追加

---
+++ JobManage/Logic/IButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobManage.Logic
+{
+    public abstract class IButton
+    {
+        /// <summary>
+        /// ボタン押下時のメイン処理
+        /// </summary>
+        public abstract void OnClickLogic();
+
+        /// <summary>
+        /// 各ボタンの状態を制御
+        /// </summary>
+        public abstract void ButtonStateChange();
+    }
+}
